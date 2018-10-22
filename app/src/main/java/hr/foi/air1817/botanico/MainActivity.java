b17ed761afdbf5aa127e448902f83a83a72400c1
Paskal Šimec <psimec@foi.hr>
2018-10-22 22:24:16 +0200

    Inicijalizaraj Firebase i napravi testne funkcije
    Inicijaliziran je Firebase i testirani su načini dohvačanja i slanja podataka u Firebase Realtime DB

--- app/src/main/java/hr/foi/air1817/botanico/MainActivity.java
+++ app/src/main/java/hr/foi/air1817/botanico/MainActivity.java
@@ -1,13 +1,57 @@
 package hr.foi.air1817.botanico;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.HashMap;
+import java.util.Map;
+
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button btnAddValue;
+    private TextView textView;
+
+    private int testInt = 0;
+
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference myRef = database.getReference("message");
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textView = (TextView)findViewById(R.id.valueView);
+        btnAddValue = (Button) findViewById(R.id.addValue);
+        btnAddValue.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Write a message to the database
+                myRef.setValue(++testInt);
+    }
+});
+
+        myRef.addValueEventListener(new ValueEventListener() {
+
+        @Override
+        public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+            String value = dataSnapshot.child("message").getValue(String.class);
+            textView.setText(value);
+        }
+
+        @Override
+        public void onCancelled(@NonNull DatabaseError databaseError) {
+
+        }
+    });
     }
 }
