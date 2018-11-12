70a4c1c20faa4efbbab449ca55e198890d876865
Mirna Tepeš <mtepes@foi.hr>
2018-11-12 13:19:11 +0100

    Dodaj ImageButton za odabir slike
    Dodan je ImageButton na zaslon za unos nove biljke, te je dodan onClick event

--- app/src/main/java/hr/foi/air1817/botanico/AddGardenActivity.java
+++ app/src/main/java/hr/foi/air1817/botanico/AddGardenActivity.java
@@ -2,6 +2,7 @@ package hr.foi.air1817.botanico;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class AddGardenActivity extends AppCompatActivity {
 
@@ -9,7 +10,11 @@ public class AddGardenActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_garden);
-    }
 
+
+    }
+    private void chooseImage(View view){
+
+    }
 
 }
